7c25cf63d09df4b2d2a704f0bdcba3f46ae6ffa2
Edy Cu <edy.cu@live.com>
2026-04-03 12:35:55 +0700

    Disable no-unused-vars rule in mapping-edge.test.tsx

--- components/mapping-edge.test.tsx
+++ components/mapping-edge.test.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars */
 import { render } from "@testing-library/react";
 import { ConfidenceEdge } from "./mapping-edge";
 
